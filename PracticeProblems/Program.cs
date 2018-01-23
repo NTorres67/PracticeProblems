3f1f2d6c3d334de0b76322b7baa0827bf28e032c
NTorres67 <ntorres67@sbcglobal.net>
2018-01-22 19:53:57 -0500

    Initial Commit

---
+++ PracticeProblems/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("What is your full name?");
+            //string fullName =
+            //fullName = "Nicole Torres";
+            //Console.WriteLine(fullName);
+
+            //Console.WriteLine("What is your favorite car?");
+            //string car =
+            //car = "Toyota Tacoma";
+            //Console.WriteLine(car);
+
+            //Console.WriteLine("Who is your comic book hero?");
+            //string comicBookHero =
+            //comicBookHero = "Hot Stuff";
+            //Console.WriteLine(comicBookHero);
+
+            //HomeWork 1/17/2018
+            //int cupsSugarOne = 7;
+            //int cupsSugarTwo = 2;
+            int cupsSugarOne = 7;
+            int cupsSugarTwo = 2;
+
+            int people = 4;
+            int costEach = 13;
+
+            int totalCost = 40;
+            int itemCost = 8;
+
+            //int trevonStart = 29;
+            //int trevonEnd = 41;
+
+            //int juliaTotalMiles = 47;
+            //int juliaExtraMiles = 30;
+            //Console.WriteLine(cupsSugarOne - cupsSugarTwo);
+            //cupsSugarOne = 4;
+            //cupsSugarTwo = 13;
+            ////Console.WriteLine(cupsSugarOne * cupsSugarTwo);
+            //cupsSugarOne = 40;
+            //cupsSugarTwo = 8;
+            //Console.WriteLine(cupsSugarOne / cupsSugarTwo);
+            //cupsSugarOne = 29;
+            //cupsSugarTwo = 41;
+            //Console.WriteLine(cupsSugarTwo - cupsSugarOne);
+            //cupsSugarOne = 47;
+            //cupsSugarTwo = 30;
+            //Console.WriteLine(cupsSugarOne - cupsSugarTwo);
+            //cupsSugarOne = 12;
+            //cupsSugarTwo = 3;
+            //Console.WriteLine(cupsSugarOne / cupsSugarTwo);
+            //cupsSugarOne = 5.12;
+            //cupsSugarTwo = 27.10;
+            //Console.WriteLine(cupsSugarOne + cupsSugarTwo);
+            //Console.WriteLine(people * costEach);
+            //Console.WriteLine(totalCost / itemCost);
+            //Console.WriteLine(trevonEnd - trevonStart);
+            //Console.WriteLine(juliaTotalMiles - juliaExtraMiles);
+            //totalCost = 12;
+            //itemCost = 3;
+            //Console.WriteLine(totalCost / itemCost);
+            //double amountPaid = 5.12;
+            //double amountEnd = 27.10;
+            //Console.WriteLine(amountPaid + amountEnd);
+
+            ////01/18/2018 Homework
+            //// Muffin Assignment - indexes
+            //int sugarNeeded = (cupsSugarOne - cupsSugarTwo);//cupsSugarOne = total sugar needed, cupsSugarTwo = sugar aready added
+            //Console.WriteLine("Muffin Assignment");
+            //Console.WriteLine("Please enter your name.");
+            //string nameResponse = Console.ReadLine();
+            //Console.WriteLine("{0}, Please add {1} additional cups of sugar to the muffin mix.", nameResponse, sugarNeeded);
+            //Console.WriteLine(" ");
+
+            ////Restaurant Bill - Concatenation
+            //int totalBill = (people * costEach);//people = number of people in dinner party, costEach = cost per diner
+            //Console.WriteLine("Restaurant Assignment");
+            //Console.WriteLine("Please list the names of the four people in the dinner party.");
+            //string diners = Console.ReadLine();
+            //Console.WriteLine("The total dinner bill for " + diners + " was $" + totalBill + ".");
+            //Console.WriteLine(" ");
+
+            ////Diaper Assignment - Indexes
+            //int number = (totalCost / itemCost);//totalCost = total amount paid, itemCost = cost of each item
+            //Console.WriteLine("Diaper Assignment");
+            //Console.WriteLine("What is your favorite brand of diapers?");
+            //string brand = Console.ReadLine();
+            //Console.WriteLine("If you have ${0}, you can purchase {1} bags of {2} diapers at ${3} per bag. ", totalCost, number, brand, itemCost);
+            //Console.WriteLine(" ");
+
+            ////Trevon/Employee Assignment - Indexes           
+            //Console.WriteLine("Emloyee Assignment");
+            //Console.WriteLine("Who received payment for services rendered?");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("How much money did employee start with?");
+            ////string start = Console.ReadLine();
+            ////int startAmount = Convert.ToInt32(start);// use Parse or triParse instead
+            //int startAmount = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("What was the total amount of money the employee had after being paid?");
+            ////string end = Console.ReadLine();
+            ////int endAmount = Convert.ToInt32(end);
+            //int endAmount = int.Parse(Console.ReadLine());
+
+            //int received = (endAmount - startAmount);
+
+            //Console.WriteLine("{0} was paid ${1} for services rendered.", name, received);
+            //Console.WriteLine(" ");
+        }
+    }
+}
