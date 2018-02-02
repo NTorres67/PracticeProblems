7cc00feab745033526a37897ae262ccbf3ad806b
NTorres67 <ntorres67@sbcglobal.net>
2018-02-01 20:32:29 -0500

    Updated

--- PracticeProblems/Program.cs
+++ PracticeProblems/Program.cs
@@ -28,14 +28,14 @@ namespace PracticeProblems
             //HomeWork 1/17/2018
             //int cupsSugarOne = 7;
             //int cupsSugarTwo = 2;
-            int cupsSugarOne = 7;
-            int cupsSugarTwo = 2;
+            //int cupsSugarOne = 7;
+            //int cupsSugarTwo = 2;
 
-            int people = 4;
-            int costEach = 13;
+            //int people = 4;
+            //int costEach = 13;
 
-            int totalCost = 40;
-            int itemCost = 8;
+            //int totalCost = 40;
+            //int itemCost = 8;
 
             //int trevonStart = 29;
             //int trevonEnd = 41;
@@ -71,6 +71,8 @@ namespace PracticeProblems
             //double amountPaid = 5.12;
             //double amountEnd = 27.10;
             //Console.WriteLine(amountPaid + amountEnd);
+
+
 
             ////01/18/2018 Homework
             //// Muffin Assignment - indexes
@@ -115,6 +117,159 @@ namespace PracticeProblems
 
             //Console.WriteLine("{0} was paid ${1} for services rendered.", name, received);
             //Console.WriteLine(" ");
+
+
+
+            ////01 / 23 / 2018 Homework Working with for loops
+            ////Array 1:  Array with 25 elements
+            //int[] numberArrayOne = new int[25];
+            //for (int i = 0; i <= numberArrayOne.Length; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.WriteLine("");
+
+
+            ////Array 2: 25 element array, print every other element
+            //int[] numberArrayTwo = new int[25];
+            //for (int j = 0; j <= numberArrayTwo.Length; j++)
+            //{
+            //    if ((j % 2) == 0)
+            //    {
+            //        Console.WriteLine(j);
+            //    }
+
+            //}
+            //Console.WriteLine("");
+
+
+            ////Array 3:  Array with 25 elements, printing every other element
+            //string[] petName = new string[25];
+            //petName[0] = "Emmalani";
+            //petName[1] = "Kilo";
+            //petName[2] = "Mana"; //means power in Hawaiian
+            //petName[3] = "Bina";
+            //petName[4] = "Malia"; //means calm waters in Hawaiian
+            //petName[5] = "Runty";
+            //petName[6] = "Impy";
+            //petName[7] = "Milo"; //tree indigenous to Hawaii
+            //petName[8] = "Koa"; //tree indigenous to Hawaii
+            //petName[9] = "Girlie";
+            //petName[10] = "Oscarella";
+            //petName[11] = "Oscar";
+            //petName[12] = "Mara";
+            //petName[12] = "Tamara";
+            //petName[13] = "Buddy";
+            //petName[14] = "Ele"; //means black in Hawaiian
+            //petName[15] = "Kolohe"; //means rascal in Hawaiian
+            //petName[16] = "Makani"; //means wind in Hawaiian
+            //petName[17] = "Pupule"; //means crazy in Hawaiian
+            //petName[18] = "Lani";  //means sky in Hawaiian
+            //petName[19] = "Kuuipo"; //means sweetheart in Hawaiian
+            //petName[20] = "Hoku"; //means Star in Hawaiian
+            //petName[21] = "Niele"; //means curious in Hawaiian
+            //petName[22] = "Mardi";
+            //petName[23] = "Tremont";
+            //petName[24] = "Timmy";
+
+            //int[] numberArray = new int[25];
+            //for (int i = 0; i < numberArray.Length; i++)
+            //{
+            //    //Console.WriteLine("Please enter a numerical value for the name " + petName[i] + ".");
+            //    Console.WriteLine("Please enter a numerical value.");//create int array without a string array
+            //    numberArray[i] = int.Parse(Console.ReadLine());
+            //    Console.WriteLine("");
+            //}
+
+            //for (int i = 0; i < numberArray.Length; i++)
+            //    if ((i % 2) == 0)
+            //    {
+            //        Console.Write((i) + ", ");
+            //    }
+
+            //Console.WriteLine("");
+            //Console.WriteLine("");
+
+
+            //01/24/2018 In class work project
+            //Team 1: display first 10 natural numbers using a loop and array
+            //        sum first 10 numbers then subtract 4 from the total
+            //        Expected result: 1 2 3 4 5 6 7 8 9 10
+
+            //int[] numberArray = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            ////int totalArray = numberArray[0] + numberArray[1] + numberArray[2] + numberArray[3] + numberArray[4] + numberArray[5] + numberArray[6] + numberArray[7] + numberArray[8] + numberArray[9] + numberArray[10];
+            //int minusNumber = 4;
+            //int sum = 0;
+            //for (int i = 0; i < numberArray.Length; i++)
+            //{
+            //    Console.WriteLine(numberArray[i]);
+            //}
+
+            //for (int i = 0; i < numberArray.Length; i++)
+            //{
+            //    int result = minusNumber;
+
+            //    sum = sum + numberArray[i];
+            //    Console.WriteLine(sum);
+            //}
+            //Console.WriteLine(sum - minusNumber);
+
+            ////Team 2 Loop Assignment ........................................ TEAM 2
+            //int[] userNumbers = new int[10];
+            //int sum = 0;
+            //for (int i = 0; i < userNumbers.Length; i++)
+            //{
+            //    Console.WriteLine("Please enter a whole number for index " + i);
+            //    userNumbers[i] = int.Parse(Console.ReadLine());
+            //}
+            //for (int i = 0; i < userNumbers.Length; i++)
+            //{
+            //    sum += userNumbers[i];
+            //}
+            //Console.WriteLine("The sum of your numbers is " + sum);
+            //if (sum % 2 == 0)
+            //{
+            //    Console.WriteLine("The sum is even");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The sum is odd");
+            //}
+
+
+            ////Team 3
+            //Console.WriteLine("How many numbers would you like me to add");
+            //int input = int.Parse(Console.ReadLine());
+            //int[] numbers = new int[input];
+            //int sum = 0;
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    sum = (sum + (i + 1));
+            //}
+            //Console.WriteLine("The sum of every sequential number to " + input + " is " + sum);
+
+
+            ////Team 4 write a program C# to read 10 numbers and find their SUM AND AVERAGE.
+            //int arrayLength;
+            //double sum = 0;
+            //double average;
+            //Console.WriteLine("Enter the array length");
+            //arrayLength = int.Parse(Console.ReadLine());
+            //int[] myNumbers = new int[arrayLength];
+            //Console.WriteLine("Enter the " + arrayLength + "number");
+            //for (int i = 0; i < myNumbers.Length; i++)
+            //{
+
+            //    myNumbers[i] = int.Parse(Console.ReadLine());
+            //}
+            //for (int i = 0; i < myNumbers.Length; i++)
+            //{
+            //    sum = sum + myNumbers[i];
+            //}
+            //Console.WriteLine("Sum of " + arrayLength + " numbers is " + sum);
+            //average = sum / myNumbers.Length;
+            //Console.WriteLine("Average of " + arrayLength + " numbers is " + average);
+
         }
     }
 }
